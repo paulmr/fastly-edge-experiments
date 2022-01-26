c5e291e61d1609c03fa72bf440ce2ab02342232e
Paul Roberts <paul.roberts@theguardian.com>
2022-01-26 15:49:26 +0000

    json bulk update
    of 999 items, which is the limit

--- make-data.tcl
+++ make-data.tcl
@@ -1,7 +1,29 @@
 #!/usr/bin/env tclsh
 
 package require uuid
+package require json::write
+
+set testgroup "test-group-1"
+
+set json_file [ open data.json w ]
+set csv_file [ open data.csv w ]
+
+puts $json_file "\{\"items\": \[\n"
+
+set start 1
 
 for { set i [ lindex $argv 0 ] } { $i > 0 } { incr i -1 } {
-    puts [ uuid::uuid generate ]
+    set id [ uuid::uuid generate ]
+    puts $csv_file $id
+    if { !$start } {
+        puts $json_file ","
+    } else {
+        set start 0
+    }
+    puts $json_file [ json::write object op [ json::write string upsert ] item_key [ json::write string $id ] item_value [ json::write string $testgroup ] ]
 }
+
+puts $json_file "] }"
+
+close $json_file
+close $csv_file
