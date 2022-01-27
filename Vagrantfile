482722d6ba651725c630204cf22046be8d5ef1cd
Paul Roberts <paul.roberts@theguardian.com>
2022-01-27 13:52:54 +0000

    support running lsp server in the vm

--- Vagrantfile
+++ Vagrantfile
@@ -4,9 +4,17 @@ Vagrant.configure("2") do |config|
   config.vm.box = "debian/bullseye64"
   config.vm.provision :shell, path: "bootstrap.sh"
 
+  # fastly serve:
   config.vm.network "forwarded_port",
                     guest: 7676,
                     host: 7676,
+                    guest_ip: "127.0.0.1",
+                    host_ip: "127.0.0.1"
+
+  # assemblyscript language server
+  config.vm.network "forwarded_port",
+                    guest: 7658,
+                    host: 7658,
                     guest_ip: "127.0.0.1",
                     host_ip: "127.0.0.1"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,37 @@
 FASTLY_VERSION=1.6.0
 
 apt-get -y update
-apt-get -y upgrade
-
+# apt-get -y upgrade
 apt-get install -y curl nodejs npm yarnpkg
 
 cd /tmp
 curl -LO "https://github.com/fastly/cli/releases/download/v1.6.0/fastly_${FASTLY_VERSION}_linux_amd64.deb"
 dpkg -i ./fastly_${FASTLY_VERSION}_linux_amd64.deb
 
-rm fastly_${FASTLY_VERSION}_linux_amd64.deb
+rm -v fastly_${FASTLY_VERSION}_linux_amd64.deb
+
+## nix is neccessary for installing the language server for assemblyscript:
+##
+##    - https://github.com/Shopify/asls
+##
+##    - https://nixos.wiki/wiki/Nix_Installation_Guide
+
+apt-get -y install nix
+
+usermod -G nix-users vagrant
+
+NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-21.11.tar.gz" su vagrant -l --whitelist-environment=NIX_PATH \
+        -c 'nix-channel --add https://nixos.org/channels/nixpkgs-unstable; nix-env -i asls -f https://github.com/saulecabrera/asls/tarball/master/'
+
+if grep -q nix-profile /home/vagrant/.profile
+then
+    echo "nix already setup in profile"
+else
+    echo >>/home/vagrant/.profile <<EOF
+if [ -d "$HOME/.nix-profile/bin" ]; then
+    PATH="$HOME/.nix-profile/bin:$PATH"
+fi
+EOF
+fi
+
+# export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-21.11.tar.gz"
